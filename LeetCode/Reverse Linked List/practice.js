81511679f7ccdc44f4633da7cc2c4b6c5e9ca52d
hhuynh10 <101972803+hhuynh10@users.noreply.github.com>
2023-02-03 23:19:01 -0600

    leetCode

---
+++ LeetCode/Reverse Linked List/practice.js
@@ -0,0 +1,16 @@
+class Node {
+    constructor(data) {
+        this.data = data;
+        this.next = null;
+    }
+}
+
+const a = new Node("A");
+const b = new Node("B");
+const c = new Node("C");
+const d = new Node("D");
+const f = new Node("F");
+
+function reverseList(head){
+    
+}
