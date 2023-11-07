ee95aa2a3cdc0528b84181fd10da3e7d34765f6c
hhuynh10 <101972803+hhuynh10@users.noreply.github.com>
2023-11-07 17:31:05 -0600

    Blind75

---
+++ Blind75/125.js
@@ -0,0 +1,29 @@
+// Valid Palindrome
+function isPalindrome(s) {
+    function isAlphaNumeric(char) {
+        return (char >= '0' && char <= '9' ||
+                char >= 'a' && char <= 'z' ||
+                char >= 'A' && char <= 'Z');
+    }
+
+    let left = 0;
+    let right = s.length - 1;
+
+    while (left < right) {
+        while (left < right && !isAlphaNumeric(s[left])) {
+            left++;
+        }
+        while (left < right && !isAlphaNumeric(s[right])) {
+            right--;
+        }
+
+        if (s[left].toLowerCase() !== s[right].toLowerCase()) {
+            return false;
+        }
+
+        left++;
+        right--;
+    }
+
+    return true;
+};
