11770389ea5f47e904e8988191417e1cebbd2a18
hhuynh10 <101972803+hhuynh10@users.noreply.github.com>
2023-11-22 22:28:15 -0600

    Blind75

---
+++ Blind75/Linked List/141.js
@@ -0,0 +1,16 @@
+// Linked List Cycle
+function hasCycle (head) {
+    let slow = head
+    let fast = head
+
+    while (fast && fast.next){
+        slow = slow.next
+        fast = fast.next.next
+
+        if (slow === fast){
+            return true
+        }
+    }
+
+    return false
+};
