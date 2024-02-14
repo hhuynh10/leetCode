cea0a11634e63937a28045ae7f1a80272750b86d
hhuynh10 <101972803+hhuynh10@users.noreply.github.com>
2024-02-13 22:12:03 -0600

    study

---
+++ WebDevSimplify/PersonCard/src/App.jsx
@@ -0,0 +1,17 @@
+import PersonCard from "./PersonCard";
+import user from "./user.json";
+
+export default function App() {
+  return (
+    <div>
+      <PersonCard
+        name={user.name}
+        age={user.age}
+        phoneNumber={user.phoneNumber}
+        address={user.address}
+      >
+        Person Card:
+      </PersonCard>
+    </div>
+  );
+}
