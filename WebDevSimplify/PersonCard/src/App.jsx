57b473495b1386ad84561d9b39fcc0fe577ca99e
hhuynh10 <101972803+hhuynh10@users.noreply.github.com>
2024-02-14 22:46:40 -0600

    study

--- WebDevSimplify/PersonCard/src/App.jsx
+++ WebDevSimplify/PersonCard/src/App.jsx
@@ -10,7 +10,7 @@ export default function App() {
         phoneNumber={user.phoneNumber}
         address={user.address}
       >
-        Person Card:
+        Person Card: tdghtreg
       </PersonCard>
     </div>
   );
