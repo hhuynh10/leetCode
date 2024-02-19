5aa3f1e5a2c6948493c4bd0fdb85e57265c52c20
hhuynh10 <101972803+hhuynh10@users.noreply.github.com>
2024-02-18 21:32:06 -0600

    study

--- JavaProject/JavaProject.java
+++ JavaProject/JavaProject.java
@@ -1,6 +1,9 @@
 package JavaProject;
 
+import java.awt.Font;
+
 import javax.swing.ImageIcon;
+import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JOptionPane;
@@ -31,18 +34,31 @@ public static void main(String args[ ])
                //create any objects if needed (i.e. scanner for keyboard input)
                 ImageIcon javaBeansIcon = new ImageIcon("JavaProject/assets/javaBeansIcon.png");
 
+                ImageIcon javaBG = new ImageIcon("JavaProject/assets/javaBG.jpg");
+
+                JButton button = new JButton();
+                button.setHorizontalAlignment(JButton.CENTER);
+                button.setVerticalAlignment(JButton.BOTTOM);
+                
                 JLabel label = new JLabel();
+                label.setText("Welcome to Java Beans' Mathematical Game!");
+                label.setIcon(javaBG);
+                label.setHorizontalTextPosition(JLabel.CENTER);
+                label.setVerticalTextPosition(JLabel.CENTER);
+                label.setFont(new Font("Serif", Font.BOLD, 24));
+                label.add(button);
 
                 JFrame frame = new JFrame();
                 frame.setVisible(true);
                 frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-                frame.setLocationRelativeTo(null);
                 frame.setTitle("Java Beans' Project");
                 frame.setLayout(new FlowLayout());
                 frame.setIconImage(javaBeansIcon.getImage());
+                frame.add(label);
                 frame.pack();
-
-                JOptionPane.showOptionDialog(null, "WOuld you like to see the game rules", "Game's Rule", JOptionPane.YES_NO_OPTION, JOptionPane.INFORMATION_MESSAGE, javaBeansIcon, null, 0);
+                frame.setLocationRelativeTo(null);
+                
+                // JOptionPane.showOptionDialog(null, "WOuld you like to see the game rules", "Game's Rule", JOptionPane.YES_NO_OPTION, JOptionPane.INFORMATION_MESSAGE, javaBeansIcon, null, 0);
 
                 // JFrame frame1 = new JFrame();
                 // frame1.setVisible(true);
